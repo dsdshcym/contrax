4072e8ab401a71294dd9307915d237f6feff4588
Yiming Chen <dsdshcym@gmail.com>
2021-06-16 08:05:55 +0800

    init: remove Contrax.hello/0 and its test

--- lib/contrax.ex
+++ lib/contrax.ex
@@ -2,17 +2,4 @@ defmodule Contrax do
   @moduledoc """
   Documentation for `Contrax`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Contrax.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/contrax_test.exs
+++ test/contrax_test.exs
@@ -1,8 +1,4 @@
 defmodule ContraxTest do
   use ExUnit.Case
   doctest Contrax
-
-  test "greets the world" do
-    assert Contrax.hello() == :world
-  end
 end
